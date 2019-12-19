04779ace0d2d6baa89d0d360ae4caf7fbc50ec60
gurucharan <gururk2@gmail.com>
2019-12-19 20:08:05 +0530

    dasd

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -2,30 +2,35 @@
 
 # any future command that fails will exit the script
 set -e
-sh /home/mindfinadmin/ci-cd/test.sh
+
+
+echo "remove repo"
 # Delete the old repo
-# rm -rf /home/mindfinadmin/mindfin
- 
-# # clone the repo again
-# git clone git@gitlab.com:mindfin1/mindfin.git /home/mindfinadmin/mindfin 
+rm -rf /home/mindfinadmin/mindfin
+
+echo "clone repo"
+# clone the repo again
+git clone git@gitlab.com:mindfin1/mindfin.git /home/mindfinadmin/mindfin 
 
 
 
-# #source the nvm file. In an non
-# #If you are not using nvm, add the actual path like
-# # PATH=/home/ubuntu/node/bin:$PATH
-# #source /home/ubuntu/.nvm/nvm.sh
+#source the nvm file. In an non
+#If you are not using nvm, add the actual path like
+# PATH=/home/ubuntu/node/bin:$PATH
+#source /home/ubuntu/.nvm/nvm.sh
 
-# # stop the previous pm2
+# stop the previous pm2
+echo "pm2 stop mindfin-webapp"
+/home/mindfinadmin/.nvm/versions/node/v12.13.0/bin/pm2 stop mindfin-webapp
 
-# /home/mindfinadmin/.nvm/versions/node/v12.13.0/bin/pm2 stop mindfin-webapp
+echo "changeing to mindfin"
+cd /home/mindfinadmin/mindfin
+
+#install npm packages
+echo "Running npm install"
+/home/mindfinadmin/.nvm/versions/node/v12.13.0/bin/npm install
 
 
-# cd /home/mindfinadmin/mindfin
-
-# #install npm packages
-# echo "Running npm install"
-# /home/mindfinadmin/.nvm/versions/node/v12.13.0/bin/npm install
-
-# #Restart the node server
-# /home/mindfinadmin/.nvm/versions/node/v12.13.0/bin/pm2 start mindfin-webapp
+echo "pm2 start"
+#Restart the node server
+/home/mindfinadmin/.nvm/versions/node/v12.13.0/bin/pm2 start mindfin-webapp
